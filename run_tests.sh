5d13172191d6fac9271cb5c7fc6e7046f941dc5e
Lucas Peres <lucasperes95@gmail.com>
2021-01-06 15:31:39 -0300

    additional dir in tests and new meaning for args

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-for file in $(ls tests/$2); do
+for file in $(ls tests/$1/$2); do
     if [ ${file: -3} == ".sl" ]; then    
-        id=$(echo tests/$2/${file} | cut -d "." -f 1 | tail -c 3)
-        output=$(./$1 < tests/$2/${file} | diff tests/$2/result${id} -)
+        id=$(echo tests/$1/$2/${file} | cut -d "." -f 1 | tail -c 3)
+        output=$(./$1.out < tests/$1/$2/${file} | diff tests/$1/$2/result${id} -)
         if [ $? != 0 ]; then
             echo "===== Test failed ====="
             echo "Input: ${file}"
